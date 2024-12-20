fdca07fa3d7cd164a4d019a74538e1ed96be683c
Nguyen Thien An <annguyen3024@gmail.com>
2024-12-20 14:08:51 +0700

    Update LoginDto

--- API_Demo_Authen_Author/API_Demo_Authen_Author/Dto/LoginDto.cs
+++ API_Demo_Authen_Author/API_Demo_Authen_Author/Dto/LoginDto.cs
@@ -6,7 +6,6 @@ namespace API_Demo_Authen_Author.Dto
         public string UserName2 { get; set; }
         public string UserName3 { get; set; }
 
-        public string UserName { get; set; }
         public string PassWord { get; set; }
     }
 }
