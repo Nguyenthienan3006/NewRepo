adae4c3a3049dd6f85d41f3e6ee825bd3c5eda04
Nguyen Thien An <annguyen3024@gmail.com>
2024-12-20 14:30:39 +0700

    Auto stash before merge of "dev1-Newfeature" and "origin/dev1-Newfeature"

--- API_Demo_Authen_Author/API_Demo_Authen_Author/Dto/LoginDto.cs
+++ API_Demo_Authen_Author/API_Demo_Authen_Author/Dto/LoginDto.cs
@@ -5,6 +5,8 @@ namespace API_Demo_Authen_Author.Dto
         public string UserName { get; set; }
         public string UserName2 { get; set; }
         public string UserName3 { get; set; }
+        public string UserName4 { get; set; }
+        public string UserName5 { get; set; }
 
         public string PassWord { get; set; }
     }
